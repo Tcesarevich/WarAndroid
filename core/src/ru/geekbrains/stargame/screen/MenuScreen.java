1cbbf3c22515c42b6bd99e66525109a4cf6ee379
Tcesarevich <43114121+Tcesarevich@users.noreply.github.com>
2019-01-22 20:18:05 +0400

    change private on protected

--- core/src/ru/geekbrains/stargame/screen/MenuScreen.java
+++ core/src/ru/geekbrains/stargame/screen/MenuScreen.java
@@ -11,23 +11,32 @@ import com.badlogic.gdx.math.Vector2;
 import ru.geekbrains.stargame.base.Base2DScreen;
 
 public class MenuScreen extends Base2DScreen {
-
-    SpriteBatch batch;
+    private static final float V_LEN=1.5f;
+//    SpriteBatch batch;
     Texture myspaceship;
     Texture background;
 
     Vector2 pos;
     Vector2 v;
-    Vector2 vtouch;
+    Vector2 v2;
+    Vector2 touch;
+    Vector2 touch1;
+    Vector2 buf;
+
 
     @Override
     public void show() {
         super.show();
-        batch = new SpriteBatch();
+     //   batch = new SpriteBatch();
         background = new Texture("background.jpg");
         myspaceship = new Texture("Myship2.png");
-        pos = new Vector2(260, 0);
-        v = new Vector2(0, 2);
+//        pos = new Vector2(260, 0);
+//        v = new Vector2(0, 2);
+        pos = new Vector2(-0.5f, -0.5f);
+        v = new Vector2(0.002f, 0.002f);
+        buf=new Vector2();
+        touch1 = new Vector2();
+        touch = new Vector2();
     }
 
     @Override
@@ -36,13 +45,18 @@ public class MenuScreen extends Base2DScreen {
         Gdx.gl.glClearColor(0.5f, 0.2f, 0.3f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
-        batch.draw(background, 0, 0);
-        batch.draw(myspaceship, pos.x, pos.y);
+        batch.draw(background, -1f, -0.5f, 2f, 1f);
+        batch.draw(myspaceship, pos.x, pos.y, 0.2f, 0.2f);
         batch.end();
-//        if (Gdx.graphics.getWidth() - 256 > pos.x && Gdx.graphics.getHeight() - 256 > pos.y) {
-//            pos.add(v);
-//        }
         pos.add(v);
+        pos.add(v2);
+//        buf.set(touch);
+//        if(buf.cpy().sub(pos).len()>V_LEN) {
+//            pos.add(v);
+//        }else {
+//            pos.set(buf);
+//        }
+
     }
 
     @Override
@@ -57,37 +71,48 @@ public class MenuScreen extends Base2DScreen {
         super.dispose();
     }
 
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        Vector2 touch;
-        touch = new Vector2(screenX, Gdx.graphics.getHeight() - screenY);
-              if (pos.x == touch.x && pos.y==touch.y) {
-            v = new Vector2(0, 0);}
-            else{
-            v = touch.sub(pos);
-            v.nor();
-            v.scl(2);
-        }
+//    @Override СТАРЫЙ МЕТОД, РАБОТАЛ В ПРЕДЫДУЩЕЙ СИСТЕМЕ КООРДИНАТ
+//    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+//        Vector2 touch;
+//        touch = new Vector2(screenX, Gdx.graphics.getHeight() - screenY);
+////              if (pos.x == touch.x && pos.y==touch.y) {
+//            v = new Vector2(-1f, 0f);
+////            else{
+//            v = touch..sub(pos);
+//      //      v.nor();
+//            v.scl(2);
+//
+//            return super.touchDown(screenX, screenY, pointer, button);
+//    }
+//
+//
+//          //  System.out.println("touchDown " + screenX + " " + (Gdx.graphics.getHeight() - screenY));
+//      //  }
+@Override
+public boolean touchDown(Vector2 touch, int pointer) {
+    System.out.println("touchDown screenX = " + pos.x + " screenY = " + pos.y);
+    touch.set(pos.x, screenBounds.getHeight() - pos.y).mul(screenToWorlds);
+    v =touch.sub(pos);
+    v.nor();
+    return super.touchDown(touch, pointer);
+}
 
-            System.out.println("touchDown " + screenX + " " + (Gdx.graphics.getHeight() - screenY));
-            return super.touchDown(screenX, screenY, pointer, button);
-        }
         @Override
         public boolean keyDown ( int keycode){
             System.out.println("keyDown keycode = " + keycode);
 
             switch (keycode) {
                 case 21:
-                    v = new Vector2(-1, 0);
+                    v = new Vector2(-0.001f, 0.000f);
                     break;
                 case 20:
-                    v = new Vector2(0, -1);
+                    v = new Vector2(0.000f, -0.001f);
                     break;
                 case 22:
-                    v = new Vector2(1, 0);
+                    v = new Vector2(0.001f, 0.000f);
                     break;
                 case 19:
-                    v = new Vector2(0, 1);
+                    v = new Vector2(0.000f, 0.001f);
                     break;
             }
             v.scl(2);
