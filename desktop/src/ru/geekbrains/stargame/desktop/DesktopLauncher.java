1aa245003d5d562587a156ae55c166a418ff0179
Tcesarevich <43114121+Tcesarevich@users.noreply.github.com>
2019-02-05 19:14:56 +0400

    added files

---
+++ desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
@@ -0,0 +1,22 @@
+package ru.geekbrains.stargame.desktop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+import ru.geekbrains.Star2DGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		System.setProperty("user.name","seconduser");
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		float aspect = 3f/4f;
+		config.width = 400;
+		config.height = (int) (config.width/aspect);
+		config.resizable = false;
+		new LwjglApplication(new Star2DGame(), config);
+
+	}
+
+}
