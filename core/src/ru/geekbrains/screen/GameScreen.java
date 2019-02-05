62cceb3d9b7e24b815959910738db2cff68b10ea
Tcesarevich <43114121+Tcesarevich@users.noreply.github.com>
2019-02-05 19:06:52 +0400

    added alla

---
+++ core/src/ru/geekbrains/screen/GameScreen.java
@@ -0,0 +1,233 @@
+package ru.geekbrains.screen;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+import java.util.List;
+
+import ru.geekbrains.base.Base2DScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.pool.BulletPool;
+import ru.geekbrains.pool.EnemyPool;
+import ru.geekbrains.pool.ExplosionPool;
+import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.Star;
+import ru.geekbrains.sprite.game.Bullet;
+import ru.geekbrains.sprite.game.Enemy;
+import ru.geekbrains.sprite.game.MainShip;
+import ru.geekbrains.sprite.menu.ButtonGameOver;
+import ru.geekbrains.sprite.menu.ButtonNewGame;
+import ru.geekbrains.utils.EnemyEmitter;
+
+public class GameScreen extends Base2DScreen {
+    private Game game;
+
+    private TextureAtlas atlas;
+    private Texture bg;
+    private Background background;
+    private Star star[];
+    private MainShip mainShip;
+
+    private BulletPool bulletPool;
+    private ExplosionPool explosionPool;
+    private EnemyPool enemyPool;
+
+    private EnemyEmitter enemyEmitter;
+
+    private Music music;
+    ButtonGameOver gameOver;
+    ButtonNewGame newGame;
+
+    public GameScreen() {
+
+    }
+
+
+    @Override
+    public void show() {
+        super.show();
+
+        music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        music.setLooping(true);
+        music.setVolume(1f);
+        music.play();
+        bg = new Texture("background.jpg");
+        background = new Background(new TextureRegion(bg));
+        atlas = new TextureAtlas("textures/mainAtlas.tpack");
+
+        star = new Star[64];
+        for (int i = 0; i < star.length; i++) {
+            star[i] = new Star(atlas);
+        }
+        bulletPool = new BulletPool();
+        explosionPool = new ExplosionPool(atlas);
+        mainShip = new MainShip(atlas, bulletPool, explosionPool);
+        enemyPool = new EnemyPool(bulletPool, worldBounds, explosionPool, mainShip);
+
+
+        enemyEmitter = new EnemyEmitter(atlas, enemyPool, worldBounds);
+        gameOver = new ButtonGameOver(atlas);
+        newGame = new ButtonNewGame(atlas, game);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update(delta);
+        checkCollisions();
+        deleteAllDestroyed();
+        draw();
+    }
+
+    private void update(float delta) {
+        for (Star aStar : star) {
+            aStar.update(delta);
+        }
+        if (!mainShip.isDestroyed) {
+            mainShip.update(delta);
+        } else if (mainShip.isDestroyed) {
+            music.stop();
+            enemyPool.dispose();
+
+        }
+        bulletPool.updateActiveSprites(delta);
+        explosionPool.updateActiveSprites(delta);
+        enemyPool.updateActiveSprites(delta);
+        enemyEmitter.generate(delta);
+    }
+
+    private void checkCollisions() {
+        List<Enemy> enemyList = enemyPool.getActiveObjects();
+        for (Enemy enemy : enemyList) {
+            if (enemy.isDestroyed()) {
+                continue;
+            }
+            float minDist = enemy.getHalfWidth() + mainShip.getHalfWidth();
+            if (enemy.pos.dst2(mainShip.pos) < minDist * minDist) {
+                enemy.destroy();
+                mainShip.damage(enemy.getDamage());
+                return;
+            }
+        }
+        List<Bullet> bulletList = bulletPool.getActiveObjects();
+
+        for (Bullet bullet : bulletList) {
+            if (bullet.getOwner() == mainShip || bullet.isDestroyed()) {
+                continue;
+            }
+            if (mainShip.isBulletCollision(bullet)) {
+                mainShip.damage(bullet.getDamage());
+                bullet.destroy();
+            }
+        }
+
+        for (Enemy enemy : enemyList) {
+            if (enemy.isDestroyed()) {
+                continue;
+            }
+            for (Bullet bullet : bulletList) {
+                if (bullet.getOwner() != mainShip || bullet.isDestroyed()) {
+                    continue;
+                }
+                if (enemy.isBulletCollision(bullet)) {
+                    enemy.damage(mainShip.getDamage());
+                    bullet.destroy();
+                }
+            }
+        }
+    }
+
+    private void deleteAllDestroyed() {
+        bulletPool.freeAllDestroyedActiveSprites();
+        explosionPool.freeAllDestroyedActiveSprites();
+        enemyPool.freeAllDestroyedActiveSprites();
+    }
+
+    private void draw() {
+        Gdx.gl.glClearColor(0.5f, 0.2f, 0.3f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        background.draw(batch);
+        for (Star aStar : star) {
+            aStar.draw(batch);
+        }
+        if (!mainShip.isDestroyed()) {
+            mainShip.draw(batch);
+        } else {
+
+            gameOver.draw(batch);
+            gameOver.resize(worldBounds);
+
+            newGame.draw(batch);
+            newGame.resize(worldBounds);
+        }
+        bulletPool.drawActiveSprites(batch);
+        explosionPool.drawActiveSprites(batch);
+        enemyPool.drawActiveSprites(batch);
+        batch.end();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        for (Star aStar : star) {
+            aStar.resize(worldBounds);
+        }
+
+
+        mainShip.resize(worldBounds);
+        gameOver.resize(worldBounds);
+        newGame.resize(worldBounds);
+    }
+
+    @Override
+    public void dispose() {
+        bg.dispose();
+        atlas.dispose();
+        bulletPool.dispose();
+        explosionPool.dispose();
+        enemyPool.dispose();
+        mainShip.dispose();
+        music.dispose();
+        super.dispose();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        if (!mainShip.isDestroyed()) {
+            mainShip.keyDown(keycode);
+        }
+        return super.keyDown(keycode);
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        if (!mainShip.isDestroyed()) {
+            mainShip.keyUp(keycode);
+        }
+        return super.keyUp(keycode);
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        if (!mainShip.isDestroyed()) {
+            mainShip.touchDown(touch, pointer);
+        }
+        return super.touchDown(touch, pointer);
+    }
+
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer) {
+        if (!mainShip.isDestroyed()) {
+            mainShip.touchUp(touch, pointer);
+        }
+        return super.touchUp(touch, pointer);
+    }
+}
---
+++ core/src/ru/geekbrains/sprite/menu/ButtonNewGame.java
@@ -0,0 +1,30 @@
+package ru.geekbrains.sprite.menu;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.screen.GameScreen;
+
+public class ButtonNewGame extends ScaledTouchUpButton {
+    private Game game;
+
+    public ButtonNewGame(TextureAtlas atlas, Game game) {
+        super(atlas.findRegion("button_new_game"));
+        this.game = game;
+        setHeightProportion(0.04f);
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        setBottom(worldBounds.getBottom() + 0.02f);
+        setRight(worldBounds.getRight() - 0.02f);
+    }
+
+    @Override
+    public void action() {
+        game.setScreen(new GameScreen());
+    }
+}
+
