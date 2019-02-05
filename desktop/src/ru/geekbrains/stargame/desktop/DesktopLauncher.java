7aebc71c53648af28f85fccc369a9a22d783bd08
Tcesarevich <43114121+Tcesarevich@users.noreply.github.com>
2019-02-05 19:11:55 +0400

    added music

--- desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
@@ -1,11 +1,9 @@
 package ru.geekbrains.stargame.desktop;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-import ru.geekbrains.stargame.Star2DGame;
+import ru.geekbrains.Star2DGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
